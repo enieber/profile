580be7cf8c15719001d73e95ddde0fe4af2e2f7c
Enieber Cunha <7907068+enieber@users.noreply.github.com>
2024-12-19 15:29:05 -0300

    feat: remove plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,5 @@
-import Script from 'next/script';
 import "../styles/reset.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Script
-        defer
-        data-domain="profile-enieber.vercel.app"
-        src="https://analytics.pop.coop/js/script.js"
-      />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />
 }
